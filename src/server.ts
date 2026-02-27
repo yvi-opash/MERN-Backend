d2962900046c2a330c7905b5e86bddd54d54f660
Yash <yvi.opash@gmail.com>
2026-02-27 11:00:40 +0530

    dfg

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://127.0.0.1:5173"
+      "http://127.0.0.1:5173",
+      "https://mern-frontend-mu-nine.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
